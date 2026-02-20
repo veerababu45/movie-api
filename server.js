728d16aa3e242c19d45afc61334bf94f97f4e5a5
veera babu <tamaranaveerababu45@gmail.com>
2026-02-20 17:41:50 +0530

    added 250 movies json

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 
 const app = express();
 app.use(cors());
 
-const API_KEY = "YOUR_TMDB_API_KEY";
+const movies = require("./movies.json");
 
-// root test
+// root route
 app.get("/", (req, res) => {
     res.send("Movie API running");
 });
 
-// get top rated movies
-app.get("/api/movies", async (req, res) => {
-    try {
-        const response = await axios.get(
-            `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-        );
+// get all movies
+app.get("/api/movies", (req, res) => {
+    res.json(movies);
+});
 
-        res.json(response.data.results);
+// get single movie
+app.get("/api/movies/:rank", (req, res) => {
+    const rank = parseInt(req.params.rank);
+    const movie = movies.find(m => m.rank === rank);
 
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch movies" });
+    if(movie){
+        res.json(movie);
+    } else {
+        res.status(404).json({message: "Movie not found"});
     }
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log("Server running on port", PORT);
 });
